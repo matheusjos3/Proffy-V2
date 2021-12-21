c7d5ca11b7d4f6aa12badfd96c7d39b7436eafaf
Matheus <matheusdc98@gmail.com>
2021-12-21 19:06:20 -0300

    Criando funções de autenticação

--- server/src/controllers/UserController.ts
+++ server/src/controllers/UserController.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
 import bcrypt from 'bcrypt';
 import db from '../database/connection';
+import jwt from 'jsonwebtoken';
+import 'dotenv/config';
 
 export default class UserController {
 
@@ -17,10 +19,10 @@ export default class UserController {
             const encrypted = bcrypt.hashSync(password, salt)
 
             await db('users').insert({
-                name, 
-                last_name, 
-                email, 
-                password:encrypted
+                name,
+                last_name,
+                email,
+                password: encrypted
             })
 
             return response.status(200).send();
@@ -31,4 +33,41 @@ export default class UserController {
             })
         }
     }
+
+    async login(request: Request, response: Response) {
+        const {
+            email,
+            password
+        } = request.body;
+
+        try {
+
+            const user = await db('users').where('email', email).first()
+
+            if (!user) return response.status(404).send('Usuario não encontrado.')
+
+            const passwordIsValid = bcrypt.compareSync(password, user.password)
+
+            if (!passwordIsValid) return response.status(400).send("A senha está incorreta")
+
+            const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+                expiresIn: 43200
+            })
+
+            return response.status(200).send({ auth: true, token: token })
+
+        } catch (err) {
+            return response.status(400).json({
+                error: 'Unexpected error.'
+            })
+        }
+    }
+
+    //Teste
+    async get(request: Request, response: Response) {
+        const user = await db('users')
+
+        return response.status(201).json(user)
+    }
 }
+
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import UserController from './controllers/UserController';
+import autenticate from './middlewares/auth'
 
 const routes = express.Router()
 const userController = new UserController()
 
+routes.post('/login', userController.login)
 routes.post('/user', userController.create)
+routes.get('/user', autenticate, userController.get)
 
 export default routes;
