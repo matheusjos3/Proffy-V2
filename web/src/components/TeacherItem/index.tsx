bb7be365bb456ff1c4877f4b34047894bd1979e0
Matheus <matheusdc98@gmail.com>
2022-04-16 17:39:05 -0300

    Adicionando componente TeacherItem

---
+++ web/src/components/TeacherItem/index.tsx
@@ -0,0 +1,100 @@
+import whatsappIcon from '../../assets/icons/whatsapp.svg';
+import defaultAvatar from '../../assets/default-avatar.svg';
+import convertMinutesToHours from '../../utils/convertMinutesToHours';
+import toReal from '../../utils/formatMoney';
+import './style.css';
+
+export interface Teacher {
+    avatar: string;
+    bio: string;
+    cost: number
+    id: number
+    name: string;
+    subject: string;
+    whatsapp: string;
+    schedules: ScheduleItem[]
+}
+
+interface ScheduleItem {
+    id: number,
+    week_day: number;
+    from: number;
+    to: number;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    const days = ['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta']
+
+    return (
+        <article className="teacher-item">
+            <header>
+                {
+                    teacher.avatar !== ''
+                        ?
+                        <img src={teacher.avatar} alt={teacher.name} />
+                        :
+                        <img src={defaultAvatar} alt={teacher.name} />
+                }
+
+                <div>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
+                </div>
+            </header>
+
+            <p>{teacher.bio}</p>
+
+            <div className="schedule">
+
+                {
+                    days.map((day, index) => {
+                        const available = teacher.schedules.find(
+                            classes => classes.week_day === index
+                        )
+
+                        if (available) {
+                            return (
+                                <div className="schedule-day" key={index}>
+                                    <span>Dia</span>
+                                    <strong>{day}</strong>
+                                    <span>Horário</span>
+                                    <strong>
+                                        {`
+                                            ${convertMinutesToHours(available.from)}h
+                                             - 
+                                            ${convertMinutesToHours(available.to)}h`}
+                                    </strong>
+                                </div>
+                            )
+                        } else {
+                            return (
+                                <div className="schedule-day" style={{ opacity: 0.4 }} key={index}>
+                                    <span>Dia</span>
+                                    <strong>{day}</strong>
+                                    <span>Horário</span>
+                                    <strong>-</strong>
+                                </div>
+                            )
+                        }
+                    })
+                }
+            </div>
+
+            <footer>
+                <p>Preço/Hora <strong>{toReal(teacher.cost)}</strong></p>
+                <a target='_blank'
+                    href={`https://wa.me/${teacher.whatsapp}`}
+                    rel="nore">
+                    <img src={whatsappIcon} alt="Entrar em contato" />
+                    Entrar em contato
+                </a>
+            </footer>
+        </article>
+    )
+}
+
+export default TeacherItem
