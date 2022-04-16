adfbeeda9621fd630729f982e3a40b71dd69944a
Matheus <matheusdc98@gmail.com>
2022-04-16 17:34:31 -0300

    Dados temporarios para listagem

---
+++ web/src/utils/user.ts
@@ -0,0 +1,68 @@
+export const teacher = [{
+    avatar: 'https://avatars.githubusercontent.com/u/2254731?v=4',
+    bio: 'Entusiasta das melhores tecnologias de química avançada.Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.',
+    cost: 20,
+    id: 1,
+    name: 'Diego Fernandes',
+    subject: 'Química',
+    whatsapp: '199222556633',
+    schedules: [{
+        id: 1,
+        week_day: 0,
+        from: 480,
+        to: 1080
+    },
+    {
+        id: 2,
+        week_day: 1,
+        from: 480,
+        to: 1080
+    },
+    {
+        id: 3,
+        week_day: 3,
+        from: 480,
+        to: 1080
+    }]
+}, {
+    avatar: 'https://avatars.githubusercontent.com/u/6643122?v=4',
+    bio: 'Instrutor de Educação Física para iniciantes, minha missão de vida é levar saúde e contribuir para o crescimento de quem se interessar.Comecei a minha jornada profissional em 2001, quando meu pai me deu dois alteres de 32kg com a seguinte condição: "Aprenda a fazer dinheiro com isso!"',
+    cost: 40,
+    id: 2,
+    name: 'Mayk Brito',
+    subject: 'Educação Física',
+    whatsapp: '199222556633',
+    schedules: [{
+        id: 4,
+        week_day: 0,
+        from: 480,
+        to: 1080
+    },
+    {
+        id: 5,
+        week_day: 4,
+        from: 480,
+        to: 720
+    }]
+}
+    , {
+    avatar: '',
+    bio: 'As vezes não sei nem onde eu tô, mas consigo me localizar facilmente em qualquer lugar. Tenho memória fotográfica e nunca fico perdido.Eu ensino a galera como não se perder na vida, com lições geográficas simples pra você nunca mais precisar de mapa na sua bela vida.',
+    cost: 360,
+    id: 3,
+    name: 'Tiago Luchtenberg',
+    subject: 'Geografia',
+    whatsapp: '199222556633',
+    schedules: [{
+        id: 6,
+        week_day: 1,
+        from: 720,
+        to: 1080
+    },
+    {
+        id: 7,
+        week_day: 2,
+        from: 480,
+        to: 1080
+    }]
+}]
