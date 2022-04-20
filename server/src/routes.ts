e21068951ce3f185b2840c44052dba431767ffb1
Matheus <matheusdc98@gmail.com>
2022-04-20 19:22:16 -0300

    Adicionando novas rotas

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -3,6 +3,8 @@ import express from 'express';
 import ClassesController from './controllers/ClassesController';
 import UserController from './controllers/UserController';
 import UserPasswordController from './controllers/UserPasswordController';
+import FavoriteController from './controllers/favoritesController';
+import ConnectionsController from './controllers/ConnectionsController';
 
 import autenticate from './middlewares/auth'
 
@@ -10,6 +12,8 @@ const routes = express.Router()
 const userController = new UserController()
 const classesController = new ClassesController()
 const userPasswordController = new UserPasswordController()
+const favoriteControler = new FavoriteController()
+const connectionsController = new ConnectionsController();
 
 routes.post('/login', userController.login)
 routes.post('/user', userController.create)
@@ -21,5 +25,12 @@ routes.put('/reset_password', userPasswordController.resertPassword)
 
 routes.get('/classes', classesController.index)
 routes.post('/classes', classesController.create)
+
+routes.get('/favorites', favoriteControler.index)
+routes.post('/favorites', favoriteControler.add)
+routes.delete('/favorites', favoriteControler.remove)
+
+routes.get('/connections', connectionsController.index)
+routes.post('/connections', connectionsController.create)
 
 export default routes;
