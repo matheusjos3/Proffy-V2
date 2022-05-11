05217c55aa1f4b0c01d8fd45f9847572e0ddd28e
Matheus <matheusdc98@gmail.com>
2022-05-11 16:10:40 -0300

    Movendo função de login para outra classe

--- server/src/controllers/UserController.ts
+++ server/src/controllers/UserController.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from 'express';
 import bcrypt from 'bcrypt';
 import db from '../database/connection';
-import jwt from 'jsonwebtoken';
 import 'dotenv/config';
 import convertHourToMinutes from '../utils/convertHourToMinutes';
 import { isEmpty } from '../utils/isEmpty';
@@ -35,37 +34,6 @@ export default class UserController {
             })
 
             return response.status(200).send();
-
-        } catch (err) {
-            return response.status(400).json({
-                error: 'Unexpected error.'
-            })
-        }
-    }
-
-    async login(request: Request, response: Response) {
-        const {
-            email,
-            password
-        } = request.body;
-
-        try {
-
-            const user = await db('users')
-                .where('email', email)
-                .first()
-
-            if (!user) return response.status(404).send('Usuario não encontrado.')
-
-            const passwordIsValid = bcrypt.compareSync(password, user.password)
-
-            if (!passwordIsValid) return response.status(400).send("A senha está incorreta")
-
-            const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
-                expiresIn: 43200
-            })
-
-            return response.status(200).send({ auth: true, token: token })
 
         } catch (err) {
             return response.status(400).json({
