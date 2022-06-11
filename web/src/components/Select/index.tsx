5d46315985d0813693b136ddb768dca3324e942c
Matheus <matheusdc98@gmail.com>
2022-06-11 19:30:50 -0300

    Alterando componente Select

--- web/src/components/Select/index.tsx
+++ web/src/components/Select/index.tsx
@@ -1,63 +1,26 @@
-import React, { useState } from 'react';
+import React, { SelectHTMLAttributes } from 'react';
 
 import './style.css';
 
-interface SelectProps {
+interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
     name: string;
     label: string;
-    placeholder: string,
-    changeValue?: (v: string) => void;
     options: Array<{
         value: string;
         label: string;
     }>;
 }
 
-const Select: React.FC<SelectProps> = ({ label, name, options, placeholder }) => {
-
-    const [selected, setSelected] = useState('')
-    const [active, setActive] = useState('')
-    const [isOpen, setIsOpen] = useState(false)
-
-    const open = () => setIsOpen(!isOpen)
-
-    function change(value: string, label: string) {
-        // changeValue(value)
-        setSelected(label)
-        setActive(value)
-    }
-
+const Select: React.FC<SelectProps> = ({ label, name, options, ...rest }) => {
     return (
         <div className="select-block">
             <label htmlFor={name}>{label}</label>
-
-            <div 
-                className={isOpen ? 'select open' : 'select'}
-                onClick={open}
-            >
-                <div className="selected">
-                    <input type="text" value={selected} placeholder={placeholder} readOnly />
-                </div>
-
-                <div className="options-container">
-                    {options.map(option => (
-                        <div key={option.value} className={`
-                                ${active === option.value ? 'option active' : 'option'}
-                            `}
-                            onClick={() => change(option.value, option.label)}
-                        >
-                            <span>{option.label}</span>
-                        </div>
-                    ))}
-                </div>
-            </div>
-
-            {/* <div className="select">
-                <div className="selected">
-                    <input type="text" value={selected} placeholder={placeholder} readOnly />
-                </div>
-                
-            </div> */}
+            <select value="" id={name} {...rest}>
+                <option value="" disabled hidden>Selecione uma opção</option>
+                {options.map(options => {
+                    return <option key={options.value} value={options.value}>{options.label}</option>
+                })}
+            </select>
         </div>
     );
 }
