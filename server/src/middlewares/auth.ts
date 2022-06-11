df806c2b975dc0c6a394355a32c13c7d6d489f79
Matheus <matheusdc98@gmail.com>
2022-06-11 19:40:48 -0300

    Mudando o código de status

--- server/src/middlewares/auth.ts
+++ server/src/middlewares/auth.ts
@@ -1,16 +1,16 @@
-import { Request, Response, NextFunction} from 'express';
+import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 import 'dotenv/config';
 
 function autenticate(request: Request, response: Response, next: NextFunction) {
     const token = request.headers.authorization;
 
-    if(!token) return response.status(401).send({error: "No token provided"})
+    if (!token) return response.status(401).send({ error: "No token provided" })
 
     const myToken = token.split(" ");
 
     jwt.verify(myToken[1], process.env.JWT_SECRET, (err) => {
-        if(err) return response.status(500).send({ error: "Token invalid"})
+        if (err) return response.status(401).send({ error: "Token invalid" })
 
         next()
     })
