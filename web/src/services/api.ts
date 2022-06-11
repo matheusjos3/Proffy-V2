532da574db94656d098bfe21b200992d5b1dc8de
Matheus <matheusdc98@gmail.com>
2022-06-11 19:37:47 -0300

    Adicionando tratamento para token inválido

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { getLocalStorageToken, getLocalStorageUser, setLocalStorageToken } from '../utils/storage';
 
 const api = axios.create({
     baseURL: 'http://localhost:3333',
@@ -6,11 +7,28 @@ const api = axios.create({
 
 api.interceptors.response.use(response => {
     return response
-}, err => {
-    return new Promise((resolve, reject) => {
-        console.log(err)
-        reject(err)
-    })
+}, async function (error) {
+    const originalConfig = error.config
+
+    if (error.response.status === 401 && error.config && !error.config._retry) {
+        originalConfig._retry = true
+        const storageToken = getLocalStorageToken()
+        const storageUser = getLocalStorageUser()
+
+        if (storageUser && storageToken) {
+            try {
+                const res = await api.put('/refresh-token', { oldToken: storageToken, id_user: storageUser.id })
+                setLocalStorageToken(res.data.token)
+                api.defaults.headers.common['Authorization'] = `Bearer ${res.data.token}`;
+                originalConfig.headers['Authorization'] = `Bearer ${res.data.token}`;
+                return api(originalConfig)
+            } catch (err) {
+                return Promise.reject(err)
+            }
+        }
+    }
+
+    return Promise.reject(error)
 })
 
 export default api;
